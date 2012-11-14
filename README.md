c8b593b3978cd0de85d9d42fa9ecbe5bd57b9588
greg russell <grgrssll@gmail.com>
2012-11-14 01:58:20 -0800

    initial commit from refactored code I wrote a while back

---
+++ README.md
@@ -0,0 +1,423 @@
+indexedDB
+=========
+
+A wrapper for javascript's indexedDB
+
+    TODO: clean up & optimize code
+
+    Tested in: Chrome 21
+               Firefox 14
+               IE 10 / Windows 8 metro app js
+
+Usage
+=========
+
+See full example at the bottom
+
+##Init##
+
+    var dbSchema = [{
+        name: 'animals',
+        key: {
+            keyPath: 'id'
+        },
+        indexes: [{
+            name: 'class',
+            field: 'class',
+            unique: false
+        }]
+    }];
+    
+    var db;
+    
+    var IDB = new DB({
+        name: 'dbname',
+        version: 1,
+        schema: dbSchema,
+        success: function (DBObject) {
+            // DBObject is a wrapper object to make calls with
+            // native IndexedDB object can be found at DBObject.db
+            db = DBObject;
+            // make calls with the db object here
+        },
+        error: function (supported, e) {
+            if (supported) {
+                console.log('indexedDB supported');
+                console.log(e);
+            } else {
+                console.log('indexedDB not supported');
+            }
+        }
+    });
+
+##DBObject##
+
+###Inserting###
+
+    var data = [
+        {id: 1, name: 'raccoon', class: 'mammalia'},
+        {id: 2, name: 'octopus', class: 'cephalopoda'},
+        {id: 3, name: 'ibex', class: 'mammalia'},
+        {id: 4, name: 'ant', class: 'insecta'},
+        {id: 5, name: 'whale', class: 'mammalia'},
+        {id: 6, name: 'eagle', class: 'aves'},
+        {id: 7, name: 'iguana', class: 'reptilia'},
+        {id: 8, name: 'crab', class: 'malacostraca'}
+    ];
+
+    db.insert({
+        store: 'animals',
+        data: data,
+        success: function (rowsAdded, rowsFailed, successEvent) { /* success */ },
+        error: function (rowsAdded, rowsFailed, failEvent) { /* error */ }
+    });
+
+###Deleting###
+
+####Delete by keypath (id in this example)####
+
+Delete record with id = 7
+
+    db.delete({
+        store: 'animals', 
+        id: 7,
+        success: function (deletedID, successEvent) { /* deleted */ },
+        error: function (failEvent) { /* error */ }
+    });
+
+####Delete all rows in object store####
+
+    db.clear({
+        store: 'animals',
+        success: function (successEvent) { /* deleted all records */ },
+        error: function (failEvent) { /* error */ }
+    });
+
+###Fetching###
+
+####Get record by keypath (id in this example)####
+
+Get record with id = 5
+
+    db.get({
+        store: 'animals', 
+        id: 5,
+        success: function (data, successEvent) { /* {id: 5, name: 'whale', class: 'mammalia'} */ },
+        error: function (failEvent) { /* error */ }
+    });
+
+####Get all records in an object store####
+
+    db.all({
+        store: 'animals',
+        success: function (data, successEvent) { /* Array all records in animals */ },
+        error: function (failEvent) { /* error */ }
+    });
+
+####Filtering and ordering data####
+
+Get all animals from class 'mammilia' in ascending order (column of ordering is name) limit 3
+
+    db.filter({
+        store: 'animals', 
+        key: 'class', 
+        value: 'mammilia', 
+        orderCol: 'name', 
+        order: 'asc', 
+        limit: 3,
+        success: function (data, successEvent) { /* first 3 animal with class mammilia in asc order by name */ },
+        error: function (failEvent) { /* error */ }
+    });
+
+###Counting records###
+
+OptionalKey will constrain result to items matching that key at value optionalKeyValue
+
+    db.count({
+        store: 'animals',
+        success: function (count, successEvent) { /* total number of records */ },
+        error: function (failEvent) { /* error */ }
+    });
+
+
+Documentation
+=========
+
+##Init##
+
+###Options####
+
+When initing the DB function it needs some options. It supports an option object with 4 parameters:
+
+* name: `<string>` the name of your database
+* version: `<int>` the version number, a new version number will trigger an upgrade (change schema)
+* schema: `<object>` an object as described below
+* debug: `<bool>` turn on off console logs
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+
+######success arguments######
+
+* `<object>` DBObject with methods: insert, delete, count, ...etc
+* `<event>` request success event
+
+######fail arguments######
+
+* `<bool>` whether indexedDB has support in the browser
+* `<event>` request fail event
+
+
+
+###Schema###
+
+The schema takes an array of objects. Each object represents an objectStore.
+
+Each objectStoreObject has 3 properties:
+
+    name: <string> the name you will reference the objectStore by when requesting data
+    
+    key: <object> an object describing the key. Properties: 
+                  
+        keyPath: <string> the objectStore's key
+        autoIncrement: <bool> if key auto increments (default false)
+        
+    indexes: <array> an array of objects that will be indexes in your objectStore. 
+                     Each object has 3 properties:
+        name: <string> the name of your index
+        field: <string> the actual field name in your object 
+        unique: <bool> whether it is a unique index (default false)
+
+##DBObject##
+
+On successful DB.init the success function is called with an argument of DBObject which is our custom object with several methods described below.
+
+###Properties###
+
+    db: <IDBObject> the actual object created by a successful `indexedDB.open().onsuccess` call
+
+###Methods###
+
+###insert (<object> options)###
+Insert data into objectStore
+
+######arguments######
+
+* store: `<string>` objectStore name [required],
+* data: `<array>` array of objects that you want to insert - not required, but pointless to leave out...
+* replace: `<bool>` default false. true will replace any records with the same key that already exist [optional]
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+######success arguments######
+
+* `<array>` array of successfully added items
+* `<array>` array of failed items
+* `<string>` the objectStore option passed
+* `<event>` request success event
+
+######fail arguments######
+
+* `<array>` array of successfully added items
+* `<array>` array of failed items
+* `<string>` the objectStore option passed
+* `<event>` request fail event
+
+###get (<object> options)###
+Get one record from objectStore by it's key
+
+######arguments######
+
+* store: `<string>` objectStore name [required],
+* id: `<mixed>` the id* of the row you are trying to retrieve, (*id as set by keyPath in schema) [required]
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+######success arguments######
+
+* `<object>` data you are asking for
+* `<string>` the objectStore option passed
+* `<event>` success event
+
+#####fail arguments#####
+
+* `<mixed>` the id you tried to get
+* `<string>` the objectStore option passed
+* `<event>` fail event
+    
+###all (<object> options)###
+Get all records from objectStore
+
+######arguments######
+
+* store: `<string>` objectStore name [required]
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+######success arguments######
+
+* `<array>` array of all records in the objectStore
+* `<string>` the objectStore option passed
+* `<event>` success event
+
+######fail arguments######
+
+* `<string>` the objectStore option passed
+* `<event>` fail event
+
+###delete (<object> options)###
+Delete one record from objectStore
+
+######arguments######
+
+* store: `<string>` objectStore name [required],
+* id: `<mixed>` the key you are trying to delete, can be `<int>` or `<string>` [required]
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+######success arguments######
+
+* `<mixed>` the key you deleted `<int>` or `<string>`
+* `<string>` the objectStore option passed
+* `<event>` success event
+
+######fail arguments######
+
+* `<string>` the objectStore option passed
+* `<event>` fail event
+
+###filter (<object> options)###
+Filter records in an objectStore by an index/value and optional ordering by arbitrary column/limiting
+
+######arguments######
+
+* store: `<string>` objectStore name [required],
+* key: `<string>` index you want to filter by, must be set up in the schema [required],
+* value: `<mixed>` value at that index you want to filer for [required],
+* orderCol: `<string>` column to order by [optional] * both orderColumn and order must be set for sorting,
+* order: `<enum ('asc'|'desc')>` defaults to 'asc' for ascending order [optional],
+* limit: `<int>` number of rows to limit to [optional]
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+######success arguments######
+
+* `<array>` array of data you are asking for
+* `<string>` the objectStore option passed
+* `<event>` success event
+
+######fail arguments######
+
+* `<string>` the objectStore option passed
+* `<event>` fail event
+
+###clear (<object> options)###
+Delete all rows from an objectStore
+
+######arguments######
+
+* store: `<string>` objectStore name [required]
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+######success arguments######
+
+* `<string>` the objectStore option passed
+* `<event>` success event
+
+######fail arguments######
+
+* `<string>` the objectStore option passed
+* `<event>` fail event
+
+###count (<object> options)###
+Count total records in an objectStore, with optional filtering for an index/value*
+
+######arguments######
+
+* store: `<string>` objectStore name [required],
+* key: `<string>` index to filter count to [optional]
+* value: `<string>` value of index to filter count to [optional]
+* success: `<function>` [see below for arguments]
+* error: `<function>` [see below for arguments]
+
+######success arguments######
+
+* `<int>` count
+* `<string>` the objectStore option passed
+* `<event>` success event
+
+######fail arguments######
+
+* `<string>` the objectStore option passed
+* `<event>` fail event
+
+
+Full Example
+=========
+
+    // schema
+    var dbSchema = [{
+        name: 'animals',
+        key: { keyPath: 'id'},
+        indexes: [
+            { name: 'class', field: 'class', unique: false }
+        ]
+    }];
+
+    // dbobject store
+    var db;
+
+    // init
+    var IDB = new DB({
+        name: 'dbname',
+        version: 1,
+        schema: dbSchema
+        success: function (DBObject) {
+            db = DBObject;
+            doStuff();
+        },
+        error: function (e) {
+            console.log('error initing db');
+        }
+    });
+    
+    function doStuff () {
+        // make calls with the db object here
+        var data = [
+            {id: 1, name: 'raccoon', class: 'mammalia'},
+            {id: 2, name: 'octopus', class: 'cephalopoda'},
+            {id: 3, name: 'ibex', class: 'mammalia'},
+            {id: 4, name: 'ant', class: 'insecta'},
+            {id: 5, name: 'whale', class: 'mammalia'},
+            {id: 6, name: 'eagle', class: 'aves'},
+            {id: 7, name: 'iguana', class: 'reptilia'},
+            {id: 8, name: 'crab', class: 'malacostraca'}
+        ];
+
+        db.insert({
+            store: 'animals', 
+            data: data, 
+            replace: true,
+            success: function (rowsAdded, rowsFailed, e) {
+	            // added
+	            console.log('Added ' + rowsAdded.length + ' rows to animals');
+	            
+	            // get id = 5
+	            db.get({
+	                store: 'animals', 
+	                id: 5,
+	                success: function (data, e) {
+	                    console.log(data.name + ' is in the class ' + data['class']);
+	                },
+	                error: function (id, e) {
+	                    console.log('error getting row ' + id);
+	                }
+	            });
+	        }, 
+	        error: function (rowsAdded, rowsFailed, e) {
+                // error
+                console.log(rowsFailed.length + ' rows failed insert');
+            }
+        });
+    }
---
+++ indexedDB.js
@@ -0,0 +1,421 @@
+/**
+ * 	@param <object> {
+ *		name:  <string>
+ *		schema: <object>
+ *		version: <int>
+ *		debug: <bool>
+ *		success: <function>
+ *		error: <function>
+ * 	}
+ */
+function DB ($options){
+    var self = this,
+    		// private polyfils
+		indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB,
+		IDBDatabase = window.IDBDatabase || window.webkitIDBDatabase,
+    		IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.mozIDBKeyRange,
+    		IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction,
+    		// private properties
+    		_readWrite = IDBTransaction.readwrite || 'readwrite',
+    		_dbRequest,
+    		_dbName,
+    		_version,
+    		_schema,
+    		_debug,
+    		// private methods
+    		__call,
+    		__isArray,
+    		__log,
+		__getTransaction,
+		__assert,
+		// class
+    		DBObject,
+    		// exception
+		IndexedDBException;
+
+    // Exception
+	IndexedDBException = function (message) { 
+		this.message = message; 
+	};
+	IndexedDBException.prototype.toString = function () { 
+		return 'IndexedDBException: ' + this.message; 
+	};
+    
+    // private functions
+    __call = function ($func, $context, $args) {
+        $func && ($func instanceof Function) && $func.apply($context, $args);
+    };
+    
+    __isArray = function ($arr) {
+        return (Object.prototype.toString.call($arr) === '[object Array]');
+    };
+    
+    __log = function ($msg) {
+        if (debug && console && console.log) {  console.log.apply($msg);  }
+    };
+    
+    __getTransaction = function ($db, $store, $rw) {
+        var trans;
+        try { 
+            trans = ($rw) ? $db.transaction($store, 'readwrite') : $db.transaction($store);
+        } catch ($err) { __log($err.message); }
+	    if (!trans) {
+			try { 
+		        trans = ($rw) ? $db.transaction([$store], _readWrite) : $db.transaction([$store]);
+		    } catch ($err) { __log($err.message); }
+	    }
+        return trans;
+    };
+    
+	__assert = function ($test, $msg) {
+		if (!$test) { 
+			throw new IndexedDBException($msg); 
+		}
+	};
+	
+	// validate options
+	_dbName = '' + $options.name;
+	_version = parseInt($options.version, 10);
+	_schema = $options.schema;
+	_debug = !!($options.debug);
+    		
+    __assert(_dbName.length, 'Invalid parameter: name <string>');
+    __assert(_version, 'Invalid parameter: version <int>');
+    __assert(__isArray(_schema), 'Invalid parameter: schema <array>');
+    
+    if (!indexedDB) {
+        __call($options.error, self, [false, $e]);
+        return false;
+    }
+    
+    // request indexedDB
+    _dbRequest = indexedDB.open(_dbName, _version);
+	
+	// request on error
+    _dbRequest.onerror = function ($e) {
+        __call($options.error, self, [true, $e]);
+    };
+	
+	// request on upgrade
+    _dbRequest.onupgradeneeded = function ($e) {
+        var updb = $e.target.result,
+        		objectStore,
+        		item,
+        		index;
+        for (var i = 0, ii = _schema.length; i < ii; i++) {
+            item = _schema[i];
+            objectStore = updb.createObjectStore(item.name, item.key);
+            if (__isArray(item.indexes)) {
+                for (var j = 0, jj = item.indexes.length; j < jj; j++) {
+                    index = item.indexes[j];
+                    objectStore.createIndex(index.name, index.field, { 
+						unique:  !!(index.unique)
+                    });
+                }
+                __call($options.upgrade, self, [$e]);
+            }
+        }
+    };
+	
+	// request on success
+    _dbRequest.onsuccess = function ($e) {
+        var idb = _dbRequest.result,
+			db = new DBObject(idb);
+		if (_version !== idb.version) {
+  			var setVersionRequest = idb.setVersion(_version);
+  			setVersionRequest.onerror = function ($e) {
+				__call($options.upgradeError, self, [$e]);
+  			};
+			setVersionRequest.onsuccess = function ($e) {
+				var objectStore,
+					item,
+					index;
+			    for (var i = 0, ii = schema.length; i < ii; i++) {
+			        item = schema[i];
+			        objectStore = idb.createObjectStore(item.name, item.key);
+			        if (__isArray(item.indexes)) {
+			            for (var j = 0, jj = item.indexes.length; j < jj; j++) {
+			                index = item.indexes[j];
+			                objectStore.createIndex(index.name, index.field, { 
+								unique:  !!(index.unique)
+			                });
+			            }
+		                __call($options.upgrade, self, [$e]);
+			        }
+			    }
+			    $e.target.transaction.oncomplete = function ($$e) {
+					__call($options.success, self, [db, $$e]);
+				};
+			}
+		} else {
+	        __call($options.success, self, [db, $e]);
+        }
+    };
+
+	// DBObject
+    DBObject = function ($idb) {
+        this.db = $idb;
+    };
+        
+    /**
+     *	@params <object> {
+     * 		store: <string>, 
+     * 		data: <array <object>>, 
+     * 		replace: <bool: *false*|true>,
+     * 		success: <function>,
+     * 		error: <function>
+     * 	}
+     */
+    DBObject.prototype.insert = function ($opts) {
+        var _self = this,
+			$store = $opts.store,
+			$data = $opts.data,
+			$repl = !!($opts.repl),
+			trans = __getTransaction(this.db, $store, true),
+			objectStore,
+			request,
+			added = [],
+			failed = [],
+			insert,
+			checkDone;
+        __assert(trans, 'Error inserting to ' + $store);
+        objectStore = trans.objectStore($store);
+        __assert(objectStore, 'Could not get object store ' + $store);
+        checkDone = function () {
+        		if ((added.length + failed.length) === ii) {
+		        __call($opts.error, _self, [added, failed, $e]);
+		    }
+        };
+        insert = function ($row) {
+            try {
+				request = ($repl) ? objectStore.put($row) : objectStore.add($row);
+	        } catch ($err) {
+	            failed.push($row);
+			    checkDone();
+	        }
+	        if (request) {
+				request.onsuccess = function ($e) { 
+				    added.push($row);
+			        checkDone();
+				};
+				request.onerror = function ($e) { 
+				    failed.push($row);
+			        checkDone();
+				};
+	        }
+		};
+        for (var i = 0, ii = $data.length; i < ii; i++) {
+            insert($data[i]);
+        }
+    };
+        
+    /**
+     *	@params <object> {
+     * 		store: <string>, 
+     * 		id: <mixed>, 
+     * 		success: <function>,
+     * 		error: <function>
+     * 	}
+     */
+    DBObject.prototype.delete = function ($opts) {
+        var _self = this,
+			$store = $opts.store,
+			$id = $opts.id,
+			trans = __getTransaction(this.db, $store, true),
+        		objectStore,
+        		request;
+        __assert(trans, 'Error deleting ' + $id + ' from ' + $store);
+        objectStore = trans.objectStore($store);
+        __assert(objectStore, 'Could not get object store ' + $store);
+        request = objectStore.delete($id);
+        request.onsuccess = function ($e) {
+            __call($opts.success, _self, [$id, $e]);
+        };
+        request.onerror = function ($e) {
+            __call($opts.error, _self, [$id, $e]);
+        };
+    };
+    
+    /**
+     *	@params <object> {
+     * 		store: <string>, 
+     * 		id: <mixed>, 
+     * 		success: <function>,
+     * 		error: <function>
+     * 	}
+     */
+    DBObject.prototype.get = function ($opts) {
+        var _self = this,
+			$store = $opts.store,
+			$id = $opts.id,
+			trans = __getTransaction(this.db, $store),
+        		objectStore,
+        		request;
+        __assert(trans, 'Error getting ' + $id + ' from ' + $store);
+        objectStore = trans.objectStore($store);
+        __assert(objectStore, 'Could not get object store ' + $store);
+		request = objectStore.get($id);
+        request.onsuccess = function ($e) {
+            __call($opts.success, _self, [request.result, $store, $e]);
+        };
+        request.onerror = function ($e) {
+            __call($opts.error, _self, [$id, $store, $e]);
+        };
+    };
+    
+    /**
+     *	@params <object> {
+     * 		store: <string>, 
+     * 		success: <function>,
+     * 		error: <function>
+     * 	}
+     */
+    DBObject.prototype.all = function ($opts) {
+        var _self = this,
+			$store = $opts.store,
+			trans = __getTransaction(this.db, $store),
+        		objectStore,
+        		request,
+        		cursor,
+        		data = [];
+        __assert(trans, 'Error getting all from ' + $store);
+        objectStore = trans.objectStore($store);
+        __assert(objectStore, 'Could not get object store ' + $store);
+        request = objectStore.openCursor();
+        request.onsuccess = function ($e) {
+            cursor = $e.target.result;
+            if (cursor) {
+                data.push(cursor.value);
+                cursor.continue();
+            } else {
+                __call($opts.success, _self, [data, $store, $e]);
+            }
+        };
+        request.onerror = function ($e) {
+            __call($opts.error, _self, [$store, $e]);
+        };
+    };
+    
+    /**
+     *	@params <object> {
+     * 		store: <string>, 
+     * 		key: <string>, 
+     * 		value: <mixed>, 
+     * 		orderCol: <string>, 
+     * 		order: <enum: (*'asc'*|'desc')>, 
+     * 		limit: <int>, 
+     * 		success: <function>,
+     * 		error: <function>
+     * 	}
+     */
+    DBObject.prototype.filter = function ($opts) {
+        var _self = this,
+			$store = $opts.store,
+			$key = $opts.key,
+			$value = $opts.value,
+			$order = $opts.order,
+			$orderCol = $opts.orderCol,
+			$limit = $opts.limit,
+			trans = __getTransaction(this.db, $store),
+			objectStore,
+			request,
+			index,
+			keyRange,
+			cursor,
+			data = [];
+        __assert(trans, 'Error filtering ' + $key + ' for ' + $value + ' from ' + $store);
+        objectStore = trans.objectStore(store);
+        __assert(objectStore, 'Could not get object store ' + $store);
+        index = objectStore.index($key);
+        keyRange = IDBKeyRange.only($value);
+        request = index.openCursor(keyRange);
+        request.onsuccess = function ($e) {
+            cursor = $e.target.result;
+            if (cursor) {
+                data.push(cursor.value);
+                cursor.continue();
+            } else {
+                if ($order && $orderCol) {
+                    data.sort(function (a, b) {
+                        var aa = a[$orderCol],
+                        		bb = b[$orderCol];
+                        return (aa < bb) ? -1 : ((aa > bb) ? 1 : 0);
+                    });
+                    if ($order === 'desc') { 
+						data.reverse(); 
+					}
+                }
+                if ($limit) {
+					data = data.slice(0, $limit);
+                }
+                __call($opts.success, _self, [data, $store, $e]);
+            }
+        };
+        request.onerror = function ($e) {
+            __call($opts.error, _self, [$store, $e]);
+        };
+    };
+    
+    /**
+     *	@params <object> {
+     * 		store: <string>, 
+     * 		success: <function>,
+     * 		error: <function>
+     * 	}
+     */
+    DBObject.prototype.clear = function ($opts) {
+		var _self = this,
+			$store = $opts.store,
+			trans = __getTransaction(this.db, $store, true),
+        		objectStore,
+        		request;
+        __assert(trans, 'Error clearing ' + $store);
+        objectStore = trans.objectStore($store);
+        __assert(objectStore, 'Could not get object store ' + $store);
+        request = objectStore.clear();
+        request.onsuccess = function ($e) {
+            __call($opts.success, _self, [$store, $e]);
+        };
+        request.onerror = function ($e) {
+            __call($opts.error, _self, [$store, $e]);
+        };
+    };
+    
+    /**
+     *	@params <object> {
+     * 		store: <string>, 
+     * 		key: <string>, 
+     * 		value: <mixed>, 
+     * 		success: <function>,
+     * 		error: <function>
+     * 	}
+     */
+    DBObject.prototype.count = function ($opts) {
+        var _self = this,
+			$store = $opts.store,
+			$key = $opts.key,
+			$value = $opts.value,
+			trans = __getTransaction(this.db, $store, true),
+        		objectStore,
+        		request,
+        		count = 0,
+        		index,
+        		keyRange;
+        __assert(trans, 'Error counting ' + $store);
+        objectStore = trans.objectStore($store);
+        __assert(objectStore, 'Could not get object store ' + $store);
+        if ($key && $value) {
+	        index = objectStore.index($key);
+	        keyRange = IDBKeyRange.only($value);
+	        request = index.count(keyRange);
+        } else {
+            request = objectStore.count();
+        }
+        request.onsuccess = function ($e) {
+            __call($opts.success, _self, [$e.target.result, $store, $e]);
+        };
+        request.onerror = function (e) {
+            __call($opts.error, _self, [$store, $e]);
+        };
+    };
+};
